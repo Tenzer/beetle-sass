6d543911a705d77d9d83ebdd82976fa226fb5b26
Jeppe Toustrup <jeppe@tenzer.dk>
2014-12-26 12:51:59 +0100

    Added the output_style argument to the plugin configuration

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 This plugin both works as a content renderer plugin and an includer handler, so any `.sass` or `.scss` files in your content or include folder will be rendered to CSS files when this plugin is enabled.
 
 Note however that you need to specify the destination file name in the YAML for a SASS file in the content folder, where as files in the include folder simply will have their file extension replaced with `.css`.
+
+You can specify one extra argument to the plugin in your `config.yaml` file: `output_style` which is then sent to the SASS compiler to specify how you want it to output the final CSS. If you for instance want a compressed/minified output you should add `output_style: compressed` to your configuration file. The available options are `nested` (the default), `expanded`, `compact` and `compressed`.
--- beetle_sass/__init__.py
+++ beetle_sass/__init__.py
@@ -3,17 +3,22 @@ from os import path
 import sass
 
 
-def render_sass(raw_content):
-    return sass.compile(string=raw_content)
+class BeetleSass(object):
+    def __init__(self, config):
+        self.output_style = config.get('output_style', 'nested')
 
+    def compile(self, content):
+        return sass.compile(string=content, output_style=self.output_style)
 
-def includer_handler(content, suggested_path):
-    content = render_sass(content.decode('utf-8'))
-    file_path, _ = path.splitext(suggested_path)
-    return '{}.css'.format(file_path), content.encode('utf-8')
+    def render(self, content, suggested_path):
+        content = self.compile(content.decode('utf-8'))
+
+        file_path, _ = path.splitext(suggested_path)
+        return '{}.css'.format(file_path), content.encode('utf-8')
 
 
 def register(context, plugin_config):
     sass_extensions = ['sass', 'scss']
-    context.content_renderer.add_renderer(sass_extensions, render_sass)
-    context.includer.add(sass_extensions, includer_handler)
+    beetle_sass = BeetleSass(plugin_config)
+    context.content_renderer.add_renderer(sass_extensions, beetle_sass.compile)
+    context.includer.add(sass_extensions, beetle_sass.render)
